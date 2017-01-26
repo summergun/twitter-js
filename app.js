c950695fe8693f57e1878110478c8cf163cd6b94
Summer Guan <summerguan@Summers-MBP.home>
2017-01-25 20:58:59 -0500

    twitterbank

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const nunjucks = require("nunjucks");
 const app = express();
 
 //connecting express with nunjuncks
-app.set("view engine", "html");
-app.engine('html', nunjucks.render);
-nunjucks.configure('views', { noCache: true });
+app.set("view engine", "html"); //the default view engine to html, so we don't have to specify on every render
+app.engine('html', nunjucks.render); //to use nunjucks.reder as the function to actually render html
+nunjucks.render('index.html', locals, function (err, output) {
+    console.log(output);
+});
+
+
+nunjucks.configure('views', { noCache: true });//point nunjucks to the proper directory for templates,cache off
 app.get('/', (req, res, next)=> {
   res.render('index', { hello: 'summer'});
 });
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,3 @@
-<h1>{{ title }}</h1>
-<p>This is a bare-bones template.</p>
-<ul>
-{% for person in people %}
-  <li>{{ person.name }}</li>
-{% endfor %}
-</ul>
+const express = require('express');
+const router = express.Router();
+const tweetBank = require('../tweetBank');
